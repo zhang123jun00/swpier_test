b348e5948f98dcb78af419d178fdc33b6a8d7bcc
zjl <zhangjunlei.001@163.com>
2024-07-08 21:51:49 +0800

    添加worker

--- user/logic.py
+++ user/logic.py
@@ -1,4 +1,12 @@
 import random
+import time
+from worker import call_by_worker
 def get_code(num):
     code = random.randrange(10**(num-1),10**num - 1)
     return  code
+
+@call_by_worker
+def send_verify_code():
+    time.sleep(5)
+    return get_code(4)
+
--- worker/config.py
+++ worker/config.py
@@ -5,10 +5,11 @@ timezone = 'Asia/Shanghai'
 accept_content = ['pickle', 'json']
 
 task_serializer = 'pickle'
-result_expires = 3600
+
 
 result_backend = 'redis://127.0.0.1:6309:0'
 result_serializer = 'pickle'
 result_cache_max = 10000
+result_expires = 3600
 
 worker_redirect_stdouts_level = 'INFO'
