faa64492eae0afc8e72060b32e8d26211259065f
zjl <zhangjunlei.001@163.com>
2024-07-08 21:32:48 +0800

    提交worker

---
+++ worker/config.py
@@ -0,0 +1,14 @@
+broker_url = '127.0.0.1:6379:0'
+broker_pool_limit = 1000
+
+timezone = 'Asia/Shanghai'
+accept_content = ['pickle', 'json']
+
+task_serializer = 'pickle'
+result_expires = 3600
+
+result_backend = 'redis://127.0.0.1:6309:0'
+result_serializer = 'pickle'
+result_cache_max = 10000
+
+worker_redirect_stdouts_level = 'INFO'
