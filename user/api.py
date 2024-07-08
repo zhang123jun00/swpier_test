32bf5553df13d0f8d36f1120be49b398bc58de0d
zjl <zhangjunlei.001@163.com>
2024-07-08 15:32:21 +0800

    修改文件

---
+++ user/api.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def get_verify_code(request):
+    '''手机注册'''
+    pass
+
+def login(request):
+    '''短信验证码登录'''
+    pass
+
+def get_profile(request):
+    '''获取个人资料'''
+    pass
+
+def modify_profile(request):
+    '''修改个人资料'''
+    pass
+
+def upload_avatar(request):
+    '''上传头像'''
+    pass
